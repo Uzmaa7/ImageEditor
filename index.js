5337be09c1bb2ceb3c97736b68c1ec471fb11ccf
Uzmaa7 <uk9971khan@gmail.com>
2026-01-14 19:01:23 +0530

    Start building filter UI dynamically from JS

---
+++ index.js
@@ -0,0 +1,90 @@
+const filters = {
+    brightness : {
+        value : 100,
+        min : 0,
+        max : 200,
+        unit: "%"
+    },
+    contrast : {
+        value : 100,
+        min : 0,
+        max : 200,
+        unit: "%"
+    },
+    exposure : {
+        value : 100,
+        min : 0,
+        max : 200,
+        unit: "%"
+    },
+    saturation : {
+        value : 100,
+        min : 0,
+        max : 200,
+        unit: "%"
+    },
+    hueRotation : {
+        value : 0,
+        min : 0,
+        max : 300,
+        unit: "deg"
+    },
+    blur : {
+        value : 0,
+        min : 0,
+        max : 20,
+        unit: "px"
+
+    },
+    greyscale :{
+        value : 0,
+        min : 0,
+        max : 100,
+        unit: "%"
+    },
+    sepia : {
+        value : 0,
+        min : 0,
+        max : 100,
+        unit: "%"
+    },
+    opacity : {
+        value : 100,
+        min : 0,
+        max : 100,
+        unit: "%"
+    },
+    invert : {
+        value : 0,
+        min : 0,
+        max : 100,
+        unit: "%"
+    }
+}
+
+function createFilterElement(name , unit = "%", value , min , max){
+
+    const div = document.createElement("div");
+    div.classList.add("filter");
+
+    const input = document.createElement("input");
+    input.type = "range";
+    input.min = min;
+    input.max = max;
+    input.id = name;
+
+    const p = document.createElement("p");
+    p.innerText = name;
+
+    div.appendChild(p);
+    div.appendChild(input);
+
+    return div;
+}
+
+
+//return an array with all the keys in string form ['brightness' 'contrast'...........]
+Object.keys(filters).forEach(filter => {
+
+    filters[filter]
+})
